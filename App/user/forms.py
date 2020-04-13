acbe5224e96d252b14f8250ea196a21c27f68d41
Ralph <ralphsliger1@gmail.com>
2020-04-13 03:59:14 -0500

    vista signup y formulario flaskwtf

---
+++ App/user/forms.py
@@ -0,0 +1,12 @@
+from wtforms import Form, StringField, PasswordField, validators
+from wtforms.fields.html5 import EmailField
+from user.models import User
+
+class RegistrationForm(Form):
+    username = StringField('Your Username', [validators.DataRequired(),validators.Length(min=2,max=30)])
+    name = StringField('Your Name', [validators.DataRequired(),validators.Length(min=2,max=30)])
+    lastname = StringField('Your Name', [validators.DataRequired(),validators.Length(min=2,max=30)])
+    city = StringField('Your City', [validators.DataRequired(),validators.Length(min=2,max=30)])
+    email = EmailField('Email Adress', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('New password',[validators.DataRequired(), validators.EqualTo('confirm', message='password must match')])
+    confirm = PasswordField('Repeat password')
--- App/user/views.py
+++ App/user/views.py
@@ -1,13 +1,22 @@
 #controlador de modulo user
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 from user.models import User
+from user.forms import RegistrationForm
+
 
 user_page = Blueprint('user_page', __name__)
 
 @user_page.route('/login')
 def login():
-    user = User(username='ralphsliger',name='ralph',lastname='sliger',city='cartagena',email='r@r.com',password='123')
-    user.save()
-    return 'Hello {}!, {} {} from {} with {}!'.format(user.username, user.name, user.lastname, user.city, user.email)
+    #user = User(username='ralphsliger',name='ralph',lastname='sliger',city='cartagena',email='r@r.com',password='123')
+    #user.save()
+    #return 'Hello {}!, {} {} from {} with {}!'.format(user.username, user.name, user.lastname, user.city, user.email)
+    return render_template('base.html')
 
+@user_page.route('/signup',methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return 'formulario'
+    return render_template('user/signup.html', form=form)
 
