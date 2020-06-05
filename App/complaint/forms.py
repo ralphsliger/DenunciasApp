6bc1334f19376a8383ffa95ceb80f78fce8b380b
Ralph <ralphsliger1@gmail.com>
2020-06-05 02:06:13 -0500

    updateds

--- App/complaint/forms.py
+++ App/complaint/forms.py
@@ -8,9 +8,9 @@ from flask_wtf.file import FileField, FileAllowed
 class BasicComplaintForm(Form):
     name = StringField('Nombre Denuncia', validators=[validators.DataRequired(), validators.Length(min=2, max=80)])
     gplace = StringField('Google Place Api')
+    place = StringField('Place', validators=[validators.DataRequired()], widget= TextArea())
     lng = FloatField('Longitude', validators=[validators.Optional()])
     lat = FloatField('Latitude', validators=[validators.Optional()])
-    place = StringField('Place', validators=[validators.DataRequired()], widget= TextArea())
     description = StringField('Descripcion' , validators=[validators.DataRequired()])
 
 
