34c97a8a273e1fc861a6d56fc7952967871341df
Ralph <ralphsliger1@gmail.com>
2020-06-05 15:56:41 -0500

    Google maps api integrate

--- App/complaint/forms.py
+++ App/complaint/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, PasswordField, validators, ValidationError,  FloatField
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField, PasswordField, validators, ValidationError,  FloatField
 from user.models import User
 from wtforms.widgets import TextArea
 from flask_wtf.file import FileField, FileAllowed
 
-class BasicComplaintForm(Form):
-    name = StringField('Nombre Denuncia', validators=[validators.DataRequired(), validators.Length(min=2, max=80)])
+class BasicComplaintForm(FlaskForm):
+    name = StringField('Nombre Denuncia', validators=[validators.DataRequired()])
     gplace = StringField('Google Place Api')
-    place = StringField('Place', validators=[validators.DataRequired()], widget= TextArea())
+    place = StringField('Place', widget= TextArea())
     lng = FloatField('Longitude', validators=[validators.Optional()])
     lat = FloatField('Latitude', validators=[validators.Optional()])
     description = StringField('Descripcion' , validators=[validators.DataRequired()])
 
-
+class EditComplaintForm(BasicComplaintForm):
+    photo = FileField('Complaint Photo', [FileAllowed(['jpg','jpeg','png'], 'Only allowed .jpg .png files')])
--- App/complaint/views.py
+++ App/complaint/views.py
@@ -1,10 +1,12 @@
 ### Controlador Modulo Complaint
 
 from flask import Blueprint, render_template, request, session, redirect, url_for, abort 
-from complaint.forms import BasicComplaintForm
+import bson
+from complaint.forms import BasicComplaintForm, EditComplaintForm
 from user.decorators import login_required
 from complaint.models import Complaint
 from user.models import User
+from utilities.storage import upload_image_file 
 
 
 
@@ -16,9 +18,9 @@ def create():
     form = BasicComplaintForm()
     error = None
     if request.method == 'POST' and form.validate():
+        error= "An error has ocurred"
         if not error:
             user = User.objects.filter(email=session.get('email')).first()
-
             complaint= Complaint(
                 name=form.name.data,
                 place=form.place.data,
@@ -28,6 +30,37 @@ def create():
                 follow= [user]
             )
             complaint.save()
-    
+            return redirect(url_for('complaint_page.edit', id=complaint.id))
     return render_template('complaint/create.html', form=form)
 
+
+
+@complaint_page.route('/<id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    #validar que el id este en la bd
+    try:
+        complaint = Complaint.objects.filter(id=bson.ObjectId(id)).first()
+    except bson.error.InvalidadId:
+        abort(404)
+    #validar que el usuario con ese id haya iniciado sesion
+    user = User.objects.filter(email=session.get('email')).first()
+    
+    if complaint and complaint.complainer == user.id:
+        error = None
+        message = None
+        form = EditComplaintForm(obj=complaint)
+        if request.method == 'POST' and form.validate():
+            error = 'An error has ocurred'
+            if not error:
+                form.populate_obj(complaint)
+                if form.lng.data and form.lat.data:
+                    complaint.location = [form.lng.data, form.lat.data]
+                image_url = upload_image_file(request.files.get('photo'), 'complaint_photo', str(complaint.id)) 
+                if image_url:
+                    complaint.complaint_photo = image_url
+                complaint.save()
+                message='Complaint Updated'
+        return render_template('complaint/edit.html', form=form, error=error, message=message, complaint=complaint)
+    else:
+        abort(404)
